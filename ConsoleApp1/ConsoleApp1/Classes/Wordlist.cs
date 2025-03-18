942c19e00e1d0443360d92ad2df39224be97114e
Hagen Utikal <Hagen.Utikal@drheuer.de>
2025-03-18 16:01:58 +0100

    AddedWordListClass

---
+++ ConsoleApp1/ConsoleApp1/Classes/Wordlist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordle.Classes
+{
+    public class Wordlist
+    {
+        public List<String> Words;
+        public string Path;
+
+        //Constructor that uses a relative Path to the File
+        public Wordlist()
+        {
+            Words = new List<String>();
+            Path = "data\\words.txt";
+            ReadListFromFile();
+        }
+
+        // Constructor that uses a CustomPath to the File
+
+        public Wordlist(string path)
+        {
+            Words = new List<String>();
+            Path = path;
+            ReadListFromFile() ;
+        }
+
+
+
+        //Method to get comparative wordlist from File in Path
+        public void ReadListFromFile()
+        {
+            StreamReader sr = new StreamReader(Path);
+            while (!sr.EndOfStream)
+            {
+                string temp = sr.ReadLine().Trim();
+                Words.Add(temp);
+            }
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Wordle.Classes;
+
+Wordlist test = new Wordlist();
+Console.WriteLine(test.Words[0]);
+Console.ReadKey();
